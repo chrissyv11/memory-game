90d2bb0918e481190daf17701b82add9b93edede
Christine <cvendikos@gmail.com>
2018-10-14 16:11:29 -0500

    Add open and show to classlist

--- js/app.js
+++ js/app.js
@@ -38,6 +38,7 @@ function shuffle(array) {
 var allCards = document.querySelectorAll('.card');
 allCards.forEach(function(card){
   card.addEventListener('click',function(e){
+    card.classList.add('open','show');
     console.log(e);
   })
 });
